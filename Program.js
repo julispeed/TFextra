b048f3a76d78df57bd579187063f37c12c4e7f7c
julispeed <106561977+julispeed@users.noreply.github.com>
2024-10-19 13:21:24 -0300

    mtc

---
+++ Program.js
@@ -0,0 +1,9 @@
+
+const chpizza= document.getElementById('Pizza');
+const dvpizza=document.getElementById('divpizza');
+
+chpizza.addEventListener('change', function()
+
+{
+    divpizza.style.display='block';
+});
